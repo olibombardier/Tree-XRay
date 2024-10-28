ae8a8073228076695baa9bc03dd01faf8ff885b3
Olivier Bombardier <olibombardier@gmail.com>
2024-10-28 19:30:33 -0400

    Realigned sprites position

--- xray-sprites.lua
+++ xray-sprites.lua
@@ -3,7 +3,7 @@ specificSprites["tree-01"] = {
   filename = "__Tree_XRay__/graphics/trees/tree-01.png",
   width = 45,
   height = 45,
-  shift = {0, -0.8},
+  shift = {0, -0.35},
   width_in_frames = 1,
   height_in_frames = 1,
 }
@@ -11,7 +11,7 @@ specificSprites["tree-02"] = {
   filename = "__Tree_XRay__/graphics/trees/tree-02.png",
   width = 38,
   height = 38,
-  shift = {0, -1},
+  shift = {0, -0.1},
   width_in_frames = 1,
   height_in_frames = 1,
 }
@@ -20,7 +20,7 @@ specificSprites["tree-03"] = {
   filename = "__Tree_XRay__/graphics/trees/tree-03.png",
   width = 52,
   height = 52,
-  shift = {0, -1},
+  shift = {0, -0.1},
   width_in_frames = 1,
   height_in_frames = 1,
 }
@@ -28,7 +28,7 @@ specificSprites["tree-04"] = {
   filename = "__Tree_XRay__/graphics/trees/tree-04.png",
   width = 58,
   height = 58,
-  shift = {0, -1},
+  shift = {0, -0.2},
   width_in_frames = 1,
   height_in_frames = 1,
 }
@@ -36,7 +36,7 @@ specificSprites["tree-05"] = {
   filename = "__Tree_XRay__/graphics/trees/tree-05.png",
   width = 31,
   height = 31,
-  shift = {0, -1},
+  shift = {0, -0.2},
   width_in_frames = 1,
   height_in_frames = 1,
 }
@@ -44,7 +44,7 @@ specificSprites["tree-06"] = {
   filename = "__Tree_XRay__/graphics/trees/tree-06.png",
   width = 55,
   height = 55,
-  shift = {0, -1},
+  shift = {0, 0},
   width_in_frames = 1,
   height_in_frames = 1,
 }
@@ -53,7 +53,7 @@ specificSprites["tree-07"] = {
   filename = "__Tree_XRay__/graphics/trees/tree-07.png",
   width = 49,
   height = 50,
-  shift = {0, -1},
+  shift = {0, -0.2},
   width_in_frames = 1,
   height_in_frames = 1,
 }
@@ -61,7 +61,7 @@ specificSprites["tree-08"] = {
   filename = "__Tree_XRay__/graphics/trees/tree-08.png",
   width = 46,
   height = 46,
-  shift = {0, -1},
+  shift = {0, -0.25},
   width_in_frames = 1,
   height_in_frames = 1,
 }
@@ -71,7 +71,7 @@ specificSprites["tree-09"] = {
   filename = "__Tree_XRay__/graphics/trees/tree-09.png",
   width = 87,
   height = 86,
-  shift = {0, -2},
+  shift = {0, -0.5},
   width_in_frames = 1,
   height_in_frames = 1,
 }
